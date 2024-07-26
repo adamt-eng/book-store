53e04451c2fd4af59df207c97bf58193a376ea42
adamt-eng <23P0001@eng.asu.edu.eg>
2024-07-26 12:16:57 +0300

    Added boolean function 'fileExists' & fixed formatting.

--- src/utils/FileManager.java
+++ src/utils/FileManager.java
@@ -3,29 +3,44 @@ package utils;
 import java.io.*;
 import java.util.*;
 
-public class FileManager {
-
-  public static List<String> readFile(String filePath) {
-    List<String> lines = new ArrayList<>();
-    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-      String line;
-      while ((line = br.readLine()) != null) {
-        lines.add(line);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+public class FileManager
+{
+    public static boolean fileExists(String filePath)
+    {
+        return new File(filePath).exists();
     }
-    return lines;
-  }
 
-  public static void writeFile(String filePath, List<String> lines) {
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
-      for (String line : lines) {
-        bw.write(line);
-        bw.newLine();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    public static List<String> readFile(String filePath)
+    {
+        List<String> lines = new ArrayList<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
+        {
+            String line;
+            
+            while ((line = br.readLine()) != null) lines.add(line);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return lines;
     }
-  }
+
+    public static void writeFile(String filePath, List<String> lines)
+    {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath)))
+        {
+            for (String line : lines)
+            {
+                bw.write(line);
+                bw.newLine();
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
