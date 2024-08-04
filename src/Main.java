64180535993694bf07f587863785f6150b78834d
adamt-eng <23P0001@eng.asu.edu.eg>
2024-08-04 14:36:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Welcome to our Book Store!");
-
+        System.out.println("Welcome to our Book Store!");
         Menu.showMainMenu();
     }
 }
