422f2c2d3998521837c304da44b306a5e4a2751d
mo7amedmengasu <163343079+mo7amedmengasu@users.noreply.github.com>
2024-08-05 14:08:34 +0300

    pulling the code up

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+
+import java.util.ArrayList;
+
+import models.Reader;
 import utils.Menu;
 import utils.OutputManager;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         OutputManager.printWithColor("Welcome to our Book Store!\n", "96m");
         Menu.showMainMenu();
     }
